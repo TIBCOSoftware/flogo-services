c31f26ed9f0edbe5c02df9423fea5bd1b3965111
Fabian <fcastill@tibco-support.com>
2016-08-29 19:12:11 -0500

    Update to use flogo-cicd

---
+++ build-flogo-state-service-release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+unset SCRIPT_ROOT
+readonly SCRIPT_ROOT=$(
+  unset CDPATH
+  script_root=$(dirname "${BASH_SOURCE}")
+  cd "${script_root}"
+  pwd
+)
+if [ -d "${SCRIPT_ROOT}/submodules/flogo-cicd" ]; then
+  rm -rf ${SCRIPT_ROOT}/submodules/flogo-cicd
+  git submodule update --init --remote --recursive
+  source ${SCRIPT_ROOT}/submodules/flogo-cicd/scripts/init.sh
+  # Build flogo/state-service docker image
+  pushd ${SCRIPT_ROOT}
+  # TODO: change to build_and_push() after 0.2.0
+  docker::build_file Dockerfile-flow-state-service flogo/state-service
+  popd
+fi
