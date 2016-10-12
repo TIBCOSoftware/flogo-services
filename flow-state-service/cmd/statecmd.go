ad2dbdc7c6fa0695ca8e77c2b2420766b1a6fbb0
Tracy Li <tibcotracy@gmail.com>
2016-10-12 13:33:26 -0500

    Change command arguments to -p and -addr

--- flow-state-service/cmd/statecmd.go
+++ flow-state-service/cmd/statecmd.go
@@ -6,8 +6,8 @@ import (
 )
 
 var log = logging.MustGetLogger("cmd")
-var Port = flag.String("pp", "9098", "The port of the server")
-var RedisAddr = flag.String("saddr", "localhost:6379", "The address of redis server, for example: localhost:6379")
+var Port = flag.String("p", "9098", "The port of the server")
+var RedisAddr = flag.String("addr", "localhost:6379", "The address of redis server, for example: localhost:6379")
 
 func init() {
 	log.Info("Starting parse cmd line paramters")
